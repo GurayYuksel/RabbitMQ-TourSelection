01ce3cfae1c80c57097a325ca94d7f6f26b4eb5f
Guray <Guray95@hotmail.com>
2022-09-07 16:11:00 +0200

    update

---
+++ rabbitmq-tour/EmailService/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Diagnostics;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "bookings",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Trace.WriteLine(message);
+    };
+
+    channel.BasicConsume(queue: "bookings",
+                         autoAck: true,
+                         consumer: consumer);
+}
+
+
---
+++ rabbitmq-tour/RabbitMQ-TourSelection/Pages/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RabbitMQ.Client;
+using System.Text;
+
+public interface IMessageProducer
+{
+    void SendMessage(string message);
+}
+
+public class RabbitMQProducer : IMessageProducer
+{
+    public void SendMessage(string message)
+    {
+        var factory = new ConnectionFactory { HostName = "localhost" };
+        var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+
+        channel.QueueDeclare(queue: "bookings",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "", routingKey: "bookings", body: body);
+    }
+}
+
+namespace RabbitMQ_TourSelection.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IMessageProducer _messageProducer;
+
+        public IndexModel(ILogger<IndexModel> logger, IMessageProducer messageProducer)
+        {
+            _logger = logger;
+            _messageProducer = messageProducer;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void SendMessage()
+        {
+            _messageProducer.SendMessage("Hej");
+        }
+    }
+}
